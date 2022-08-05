83f0c213762cf253de40977312d215325c20fa04
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2022-08-05 10:32:39 -0700

    Added Comic Functionality
    Imported cinemachine and added 3 scripts: SceneLoader, ComicScript, and CinemachineShake

---
+++ Sandwitch Shop/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(FindObjectOfType<Singleton>());
+    }
+    public void LoadNextScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+
+    public void LoadSceneName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

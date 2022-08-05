418b79bdca49b4dd38b83bf9402a721a700b369a
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2022-08-05 15:13:18 -0700

    Added Pause Functionality
    Also added a selector icon to show which option will be chosen in UI menus

--- Sandwitch Shop/Assets/Scripts/GameStateManager.cs
+++ Sandwitch Shop/Assets/Scripts/GameStateManager.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour
 {
+    [SerializeField] GameObject selectorIcon;
     [SerializeField] GameObject WinScreen;
     [SerializeField] GameObject LoseScreen;
     [SerializeField] GameObject PauseMenu;
 
     public void WinGame()
     {
+        selectorIcon.SetActive(true);
         WinScreen.SetActive(true);
     }
     public void LoseGame()
     {
+        selectorIcon.SetActive(true);
         LoseScreen.SetActive(true);
     }
     public void PauseGame()
     {
+        selectorIcon.SetActive(true);
         PauseMenu.SetActive(true);
     }
     public void UnpauseGame()
     {
+        selectorIcon.SetActive(false);
         PauseMenu.SetActive(false);
     }
 }
--- Sandwitch Shop/Assets/Scripts/PauseGame.cs
+++ Sandwitch Shop/Assets/Scripts/PauseGame.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 public class PauseGame : MonoBehaviour
 {
     [SerializeField] float timeToPause = 3f;
+    float timeHeldDown = 0f;
     bool tryingToPause = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        //start trying to pause if all 3 keys are held down
         if(!tryingToPause && Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("Try To Pause");
-            StartCoroutine(TryToPause());
+            tryingToPause = true;
+            //Debug.Log("Try To Pause");
+            //StartCoroutine(TryToPause());
+        }
+
+        //adds time between and eventually pauses the game when it hits the time needed to pause
+        if (tryingToPause)
+        {
+            // if any of 3 keys was released, stop trying to pause
+            if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                tryingToPause = false;
+                timeHeldDown = 0f;
+            }
+            //Add the real time that has passed between frames
+            timeHeldDown += Time.deltaTime;
+            //pause game when time held down meets time needed to pause
+            if(timeHeldDown >= timeToPause)
+            {
+                timeHeldDown = 0f;
+                FindObjectOfType<GameStateManager>().PauseGame();
+            }
         }
     }
 
-    IEnumerator TryToPause()
+   /* IEnumerator TryToPause()
     {
         tryingToPause = true;
         while (tryingToPause)
@@ -37,5 +52,5 @@ public class PauseGame : MonoBehaviour
         yield return new WaitForSeconds(timeToPause);
         tryingToPause = false;
         FindObjectOfType<GameStateManager>().PauseGame();
-    }
+    } */
 }

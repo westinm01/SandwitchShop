f4eed3affa6fcbe63a2025f682e19a360ce1bc3b
Prathmesh Jain <prathmeshjain02@gmail.com>
2022-08-06 15:25:03 +0530

    Options Menu and Sound Options

---
+++ Sandwitch Shop/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    // [Header("Volume Settings")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    //[SerializeField] private Slider volumeSlider = null;
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("volume", AudioListener.volume);
+    }
+}

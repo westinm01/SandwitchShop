853e9aa16d3a8c8e5292c52107c7702b71a2beb5
Wes <wmont012@ucr.edu>
2022-08-06 14:02:41 -0700

    Made portal fridge

--- Sandwitch Shop/Assets/Scripts/Food/Food.cs
+++ Sandwitch Shop/Assets/Scripts/Food/Food.cs
@@ -7,11 +7,18 @@ public abstract class Food : MonoBehaviour
     public bool isCuttable;
     public bool isBakable;
     public bool isBaked;
+    public bool isPunchable;
+    public bool isPunched;
     public bool isReadyForAssembly;
     // Start is called before the first frame update
     protected virtual void Start()
     {
-
+        isCuttable = false;
+        isBakable = false;
+        isBaked = false;
+        isPunchable = false;
+        isPunched = false;
+        isReadyForAssembly = false;
     }
 
     // Update is called once per frame
--- Sandwitch Shop/Assets/Scripts/Food/Veggy.cs
+++ Sandwitch Shop/Assets/Scripts/Food/Veggy.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Veggy : Food
 {
     public Ingredients.veggy veggy;
-    public bool isCut;
+    //public bool isCut;
     // Start is called before the first frame update
     protected override void Start()
     {
+        base.Start();
         isCuttable = true;
-        isCut = false;
+        //isCut = false;
     }
 
     // Update is called once per frame

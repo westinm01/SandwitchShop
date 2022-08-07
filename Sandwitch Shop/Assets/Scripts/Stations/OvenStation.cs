a2a30e7acddcbbff65e2c502a2047167fc7b1561
Wes <wmont012@ucr.edu>
2022-08-06 17:23:01 -0700

    Edited OvenStation & Added volcano

--- Sandwitch Shop/Assets/Scripts/Stations/OvenStation.cs
+++ Sandwitch Shop/Assets/Scripts/Stations/OvenStation.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class OvenStation : Station
 {
     // Start is called before the first frame update
+
+    public float timeToBake = 5f;
+    private float currentTime = 0f;
+
+    private bool isBaking = false;
+    private bool meatReady = true;
+    
     protected override void Start()
     {
         base.Start();
@@ -15,14 +22,36 @@ public class OvenStation : Station
     protected override void Update()
     {
         base.Update();
+         if(isBaking)
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime >= timeToBake)
+            {
+               
+                meatReady = true;
+                isBaking = false;
+            }
+        }
     }
 
     void Bake()
     {
-        if (player.hasFood && player.currentFood.isBakable)
+        if (Hand.getItem() != null && player.currentFood.isBakable && !isBaking && !meatReady)
         {
-            player.currentFood.isBaked = true;
-            player.hasFood = false;
+            player.hasFood = false; //need to drop item from Hand too...
+            isBaking = true;
         }
+        else if (Hand.getItem() == null && !isBaking && meatReady)
+        {
+                        currentTime = 0;
+                        meatReady = false;
+                        //player.currentFood.isBaked = true;
+                        /*GameObject newFood = new GameObject();
+                        newFood.AddComponent<Bread>();
+                        newFood.GetComponent<Bread>().bread = breads[index];
+                        Hand.setItem(newFood.GetComponent<Bread>(), iconSprites[index]);
+                        player.hasFood = true;*/
+        }
+        
     }
 }
